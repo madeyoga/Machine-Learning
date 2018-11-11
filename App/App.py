7c567a19af9fc77c366e18fc16c744f9dc72b361
MadeYoga <vngla21@gmail.com>
2018-11-11 22:31:34 +0700

    update App.py

--- App/App.py
+++ App/App.py
@@ -14,8 +14,8 @@ datasets_path       = this_script_path + "\\Datasets"
 
 os.chdir(datasets_path)
 
-training_data_name  = "heartdisease-train.csv" # "iris-train.csv"# input("input training dataset file name: ")
-test_data_name      = "heartdisease-test.csv"  # "iris-test.csv" # input("input test dataset file name : ")
+training_data_name  = "heartdisease-train.csv" # "iris-train.csv" # input("input training dataset file name: ")
+test_data_name      = "heartdisease-test.csv"  # "iris-test.csv"  # input("input test dataset file name : ")
 
 training_dataset    = pd.read_csv(training_data_name)
 test_dataset        = pd.read_csv(test_data_name)
@@ -23,10 +23,22 @@ test_dataset        = pd.read_csv(test_data_name)
 manager = Manager(training_dataset=training_dataset, test_dataset=test_dataset)
 manager.scale_data()
 
-print("KNN PREDICTIONS: ")
-print(manager.get_KNN_prediction())
-print(manager.get_KNN_accuracy())
+print(manager.get_compare_frame())
 
-print("GNB PREDICTIONS: ")
-print(manager.get_GNB_prediction())
-print(manager.get_GNB_accuracy())
+# print(manager.KNN_model)
+
+# knn_pred = manager.get_KNN_prediction()
+# pd_knn = pd.DataFrame(
+#     data={'knn predictions' : knn_pred}
+# )
+# pd_compare = manager.Y_test.join(pd_knn)
+
+# gnb_pred = manager.get_GNB_prediction()
+# pd_gnb = pd.DataFrame(
+#     data={'gnb predictions' : gnb_pred}
+# )
+# pd_compare = pd_compare.join(pd_gnb)
+# print(pd_compare)
+
+# print(manager.get_KNN_accuracy())
+# print(manager.get_GNB_accuracy())
