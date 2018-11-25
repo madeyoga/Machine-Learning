c7a68055f703590851454c020dffbf557203f71c
MadeYoga <vngla21@gmail.com>
2018-11-25 11:25:42 +0700

    added 'graph representation'

--- App/App.py
+++ App/App.py
@@ -1,9 +1,18 @@
-import time
-from Models.Classification.ClassifierModel import *
+"""
+* Neural Network model sangat sensitive pada data yg valuenya tidak di scale.
+* Cross Validation score, menggunakan rata-rata
+*
+*
+*
+*
+"""
+from Models.Classification.ClassifierModel import KNeighborsModel
+from Models.Classification.ClassifierModel import NBGaussModel
+from Models.Classification.ClassifierModel import DTreeModel
+from Models.Classification.ClassifierModel import MultiLayerPerceptronModel
 from DataManager import Manager
-
+from sklearn.model_selection import cross_val_score, KFold
 import numpy as np
-
 import pandas as pd
 import os
 
@@ -26,35 +35,52 @@ training_dataset    = pd.read_csv(training_data_filename)
 test_dataset        = pd.read_csv(test_data_filename)
 
 print("Load Dataset... ({}/{})".format(training_data_filename, test_data_filename))
-time.sleep(1)
 manager = Manager(training_dataset=training_dataset, test_dataset=test_dataset)
-manager.scale_data()
+# manager.scale_data()
 
 ## INITIAL KNN MODEL
 KNN_model = KNeighborsModel(n=5)
 KNN_model.train(X=manager.X_train, Y=manager.Y_train)
-KNN_model.predict_y(test_set_x=manager.X_test)
-
 ## INITIAL GNB MODEL
 GNB_model = NBGaussModel()
 GNB_model.train(X=manager.X_train, Y=manager.Y_train)
-## PREDICT
+## INITIAL DTREE MODEL
+DT_model = DTreeModel()
+DT_model.train(X=manager.X_train, Y=manager.Y_train)
+## INITIAL MULTI-LAYER PERCEPTRON MODEL
+MLP_model = MultiLayerPerceptronModel()
+MLP_model.train(X=manager.X_train, Y=manager.Y_train)
+## PREDICT/TESTING
 GNB_model.predict_y(test_set_x=manager.X_test)
+KNN_model.predict_y(test_set_x=manager.X_test)
+DT_model. predict_y(test_set_x=manager.X_test)
+MLP_model.predict_y(test_set_x=manager.X_test)
+
+scores_KNN = []
+scores_GNB = []
+scores_DT  = []
+scores_MLP = []
 
 print("Test Accuracy: ")
 print("KNN : {}%".format(KNN_model.get_accuracy(Y_test=manager.Y_test) * 100))
 print("GNB : {}%".format(GNB_model.get_accuracy(Y_test=manager.Y_test) * 100))
+print("DT  : {}%".format(DT_model .get_accuracy(Y_test=manager.Y_test) * 100))
+print("MLP : {}%".format(MLP_model.get_accuracy(Y_test=manager.Y_test) * 100))
 
-KNN_model.predict_y(test_set_x=manager.X_train)
-GNB_model.predict_y(test_set_x=manager.X_train)
-print("Training Accuracy with training dataset: ")
-print("KNN : {}%".format(KNN_model.get_accuracy(Y_test=manager.Y_train) * 100))
-print("GNB : {}%".format(GNB_model.get_accuracy(Y_test=manager.Y_train) * 100))
+scores_KNN.append(KNN_model.get_accuracy(Y_test=manager.Y_test) * 100)
+scores_GNB.append(GNB_model.get_accuracy(Y_test=manager.Y_test) * 100)
+scores_DT .append(DT_model .get_accuracy(Y_test=manager.Y_test) * 100)
+scores_MLP.append(MLP_model.get_accuracy(Y_test=manager.Y_test) * 100)
 
+#################################
+### TRAIN TEST SPLIT APPROACH ###
+#################################
 manager.do_train_test_split()
-GNB_model.train(X=manager.X_train, Y=manager.Y_train)
-GNB_model.predict_y(test_set_x=manager.X_train)
 
+## GNB
+GNB_model.train(X=manager.X_train, Y=manager.Y_train)
+GNB_model.predict_y(test_set_x=manager.X_test)
+## KNN
 knn_scores = []
 k_n_range = range(1, 26)
 for k in k_n_range:
@@ -62,16 +88,127 @@ for k in k_n_range:
    KNN_model.train(X=manager.X_train, Y=manager.Y_train)
    KNN_model.predict_y(test_set_x=manager.X_test)
    knn_scores.append(KNN_model.get_accuracy(Y_test=manager.Y_test))
+## DT
+DT_model.train(X=manager.X_train, Y=manager.Y_train)
+DT_model.predict_y(test_set_x=manager.X_test)
+## MLP
+MLP_model.train(X=manager.X_train, Y=manager.Y_train)
+MLP_model.predict_y(test_set_x=manager.X_test)
+
+scores_KNN.append(max(knn_scores) * 100)
+scores_GNB.append(GNB_model.get_accuracy(Y_test=manager.Y_train) * 100)
+scores_DT .append(DT_model .current_accuracy * 100)
+scores_MLP.append(MLP_model.current_accuracy * 100)
 
 print("Training Accuracy with train_test_split approach: ")
-print("KNN : {}%".format(max(knn_scores) * 100))
-print("GNB : {}%".format(GNB_model.get_accuracy(Y_test=manager.Y_train) * 100))
+print("KNN : {}%".format(int(max(knn_scores) * 100)))
+print("GNB : {}%".format(int(GNB_model.get_accuracy(Y_test=manager.Y_train) * 100)))
+
+#################################
+### CROSS VALIDATION APPROACH ###
+#################################
+manager = Manager(training_dataset=training_dataset, test_dataset=test_dataset)
+manager.scale_data()
+
+## cross val StartifiedKFold
+## KFold
+# kf = KFold(n_splits=10)
+print(cross_val_score(KNN_model.model, manager.X_train, manager.Y_train.values.ravel(), cv=10).mean() * 100)
+print(cross_val_score(GNB_model.model, manager.X_train, manager.Y_train.values.ravel(), cv=10).mean() * 100)
+print(cross_val_score(DT_model .model, manager.X_train, manager.Y_train.values.ravel(), cv=10).mean() * 100)
+print(cross_val_score(MLP_model.model, manager.X_train, manager.Y_train.values.ravel(), cv=10).mean() * 100)
+scores_KNN.append(cross_val_score(KNN_model.model, manager.X_train, manager.Y_train.values.ravel(), cv=10).mean() * 100)
+scores_GNB.append(cross_val_score(GNB_model.model, manager.X_train, manager.Y_train.values.ravel(), cv=10).mean() * 100)
+scores_DT .append(cross_val_score(DT_model.model , manager.X_train, manager.Y_train.values.ravel(), cv=10).mean() * 100)
+scores_MLP.append(cross_val_score(MLP_model.model, manager.X_train, manager.Y_train.values.ravel(), cv=10).mean() * 100)
 
 from matplotlib import pyplot as plt
 
-plt.plot(k_n_range, knn_scores)
-plt.xlabel("K")
-plt.ylabel("Score")
+n_groups = 3
+
+ax = plt.subplot()
+index = np.arange(n_groups)
+bar_width = 0.15
+opacity = 0.8
+
+print(tuple(scores_KNN))
+
+rects1 = ax.bar(
+   index, tuple(scores_KNN), bar_width,
+   alpha=opacity,
+   color='b',
+   label='KNN'
+)
+
+rects2 = ax.bar(
+   index + bar_width, tuple(scores_GNB), bar_width,
+   alpha=opacity,
+   color='g',
+   label='GNB'
+)
+
+rects3 = ax.bar(
+   index + bar_width * 2, tuple(scores_DT), bar_width,
+   alpha=opacity,
+   color='y',
+   label='DT'
+)
+
+rects4 = ax.bar(
+   index + bar_width * 3, tuple(scores_MLP), bar_width,
+   alpha=opacity,
+   color='r',
+   label='MLP'
+)
+
+
+for rect in rects1:
+   ax.text(
+      rect.get_x() + rect.get_width()/5,
+      1.01*rect.get_height(),
+      str(int(rect.get_height())) + "%", 
+      color='b', 
+      fontweight='bold'
+      )
+
+for rect in rects2:
+   ax.text(
+      rect.get_x() + rect.get_width()/5,
+      1.01*rect.get_height(),
+      str(int(rect.get_height())) + "%", 
+      color='g', 
+      fontweight='bold'
+      )
+
+for rect in rects3:
+   ax.text(
+      rect.get_x() + rect.get_width()/5,
+      1.01*rect.get_height(),
+      str(int(rect.get_height())) + "%", 
+      color='y', 
+      fontweight='bold'
+   )
+
+for rect in rects4:
+   ax.text(
+      rect.get_x() + rect.get_width()/5,   # label horizontal position
+      1.01*rect.get_height(),              # label vertical position
+      str(int(rect.get_height())) + "%", 
+      color='r', 
+      fontweight='bold'
+   )
+
+plt.ylim([0, 100])
+plt.xlabel('Model')
+plt.ylabel('Scores')
+plt.title("Scores by model {}/{} dataset".format(training_data_filename, test_data_filename))
+plt.xticks(index + bar_width * 1.5, ('Testing', 'Train Test Split', 'Cross Validation'))
+plt.legend()
+plt.tight_layout()
+
+# plt.plot(k_n_range, knn_scores)
+# plt.xlabel("K")
+# plt.ylabel("Score")
 plt.show()
 
 
--- App/Models/Classification/ClassifierModel.py
+++ App/Models/Classification/ClassifierModel.py
@@ -3,6 +3,8 @@ import os
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 
 class ClassifierModel:
@@ -35,6 +37,33 @@ class ClassifierModel:
         except ValueError as error:
             error.with_traceback(sys.exc_info()[2])
         return self.current_accuracy
+
+class DTreeModel(ClassifierModel):
+    def __init__(self):
+        self.model = DecisionTreeClassifier(random_state=0)
+
+class MultiLayerPerceptronModel(ClassifierModel):
+    """ 
+    Multi-layer is sensitive to feature scaling, 
+    so it is highly recommended to scale data.
+    scale each attribute to [0, 1] or [-1, +1]
+    or standardize it to have mean 0 and variance 1.
+    - activation : {‘identity’, ‘logistic’, ‘tanh’, ‘relu’}
+    - solver     : {‘lbfgs’, ‘sgd’, ‘adam’}, default ‘adam’
+        The solver for weight optimization.
+        ‘lbfgs’ is an optimizer in the family of quasi-Newton methods.
+        ‘sgd’ refers to stochastic gradient descent.
+        ‘adam’ refers to a stochastic gradient-based optimizer proposed by Kingma, Diederik, and Jimmy Ba
+    """
+    def __init__(self):
+        
+        self.model = MLPClassifier(
+            solver='lbfgs',
+            alpha=1e-5, # 5 nol dibelakang koma, 0.00005 # learning rate
+            activation='relu', # perceptron activation relu
+            hidden_layer_sizes=(5, 2), 
+            random_state=1,
+        )
 
 class NBGaussModel(ClassifierModel):
     def __init__(self):
